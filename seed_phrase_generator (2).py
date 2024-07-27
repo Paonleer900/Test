db4a88088456a995b9ee0d33404706e7c9fb0c0e
Paonleer900 <kinogramm900@mail.ru>
2024-07-27 19:46:44 +0600

    Созданы 50 аккаунтов с помощью seed_phrase_generator (2).py

--- seed_phrase_generator (2).py
+++ seed_phrase_generator (2).py
@@ -1,12 +1,10 @@
 import os
-# from web3 import Web3
 from eth_account import Account
 
 # Включаем непроверенные функции HD Wallet
 Account.enable_unaudited_hdwallet_features()
 
 from mnemonic import Mnemonic
-
 
 def create_wallets_bip39(num_wallets, num_words):
     # Создание директории для кошельков, если она не существует
@@ -19,7 +17,7 @@ def create_wallets_bip39(num_wallets, num_words):
     mnemo = Mnemonic("english")
     for _ in range(num_wallets):
         # Генерируем новый аккаунт
-        seed = mnemo.generate(strength=num_words * 32 // 3)
+        seed = mnemo.generate(strength=(num_words * 32) // 3)
         mnemonic_words = seed
         acc = Account.from_mnemonic(mnemonic_words)
         address = acc.address
@@ -30,9 +28,8 @@ def create_wallets_bip39(num_wallets, num_words):
 
     print(f"Generated {num_wallets} wallets in the 'wallets' folder.")
 
-
 # Количество кошельков для генерации
-num_wallets = 100
+num_wallets = 50
 # Количество слов в seed phrase
 num_words = 12
 
